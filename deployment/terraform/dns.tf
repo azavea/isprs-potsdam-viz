05f731e5b34f98d49bcb8d043ba7c51e14c1de70
Taylor Nation <tnation14@gmail.com>
2017-11-21 16:31:32 -0500

    Update terraform deployment to use Geotrellis Demo Framework
    - Use terraform-aws-ecs-service module to create new ECS task
    - ECS task logs to CloudWatch
    - Use terraform remote state to access core infrastructure resources
    - Remove unnecessary logs bucket
    - Add security rules to allow HTTP(s) to the loadbalancers, and all communication between ALB and container instances
    - Add CloudFront distribution to cache tiles

--- deployment/terraform/dns.tf
+++ deployment/terraform/dns.tf
@@ -1,18 +1,27 @@
 #
 # Route 53 Resources
 #
-resource "aws_route53_zone" "external" {
-  name = "${var.route53_external_hosted_zone}"
-}
 
-resource "aws_route53_record" "server_app" {
-  zone_id = "${aws_route53_zone.external.id}"
-  name    = "${var.route53_external_hosted_zone}"
+resource "aws_route53_record" "origin" {
+  zone_id = "${data.terraform_remote_state.core.public_hosted_zone_id}"
+  name    = "potsdam-origin.${data.aws_route53_zone.external.name}"
   type    = "A"
 
   alias {
-    name                   = "${lower(aws_alb.server_app.dns_name)}."
-    zone_id                = "${aws_alb.server_app.zone_id}"
+    name                   = "${lower(module.potsdam_ecs_service.lb_dns_name)}."
+    zone_id                = "${module.potsdam_ecs_service.lb_zone_id}"
     evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "cloudfront" {
+  zone_id = "${data.aws_route53_zone.external.id}"
+  name    = "potsdam.${data.aws_route53_zone.external.name}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_cloudfront_distribution.cdn.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+    evaluate_target_health = false
   }
 }
--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -1,18 +1,12 @@
 #
 # S3 resources
 #
-# resource "aws_s3_bucket" "logs" {
-#   bucket = "rastervision-${lower(var.environment)}-logs-${var.aws_region}"
-#   tags {
-#     Project     = "${var.project}"
-#     Environment = "${var.environment}"
-#   }
-# }
-# resource "aws_s3_bucket" "catalogs" {
-#   bucket = "rastervision-${lower(var.environment)}-catalogs-${var.aws_region}"
-#   tags {
-#     Project     = "${var.project}"
-#     Environment = "${var.environment}"
-#   }
-# }
 
+resource "aws_s3_bucket" "catalogs" {
+  bucket = "potsdam-${lower(var.environment)}-catalogs-${var.aws_region}"
+
+  tags {
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+}
