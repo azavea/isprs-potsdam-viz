a4a4099c47b1f09cb070ed587b71cc8a80bebf84
Taylor Nation <tnation14@gmail.com>
2017-11-21 16:31:32 -0500

    Update terraform deployment to use Geotrellis Demo Framework
    - Use terraform-aws-ecs-service module to create new ECS task
    - ECS task logs to CloudWatch
    - Use terraform remote state to access core infrastructure resources
    - Remove unnecessary logs bucket
    - Add security rules to allow HTTP(s) to the loadbalancers, and all communication between ALB and container instances
    - Add CloudFront distribution to cache tiles

--- deployment/terraform/app.tf
+++ deployment/terraform/app.tf
@@ -1,121 +1,51 @@
-#
-# Security group resources
-#
-resource "aws_security_group" "server_app_alb" {
-  vpc_id = "${module.vpc.id}"
-
-  # vpc_id = "${var.vpc_id}"
-
-  tags {
-    Name        = "sgAppServerLoadBalancer"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
-
-resource "aws_security_group_rule" "alb_api_server_https_ingress" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = "${var.server_app_alb_ingress_cidr_block}"
-
-  security_group_id = "${aws_security_group.server_app_alb.id}"
-}
-
-resource "aws_security_group_rule" "alb_api_server_container_instance_all_egress" {
-  type      = "egress"
-  from_port = 0
-  to_port   = 65535
-  protocol  = "tcp"
-
-  security_group_id        = "${aws_security_group.server_app_alb.id}"
-  source_security_group_id = "${aws_security_group.container_instance.id}"
-}
-
-#
-# ALB resources
-#
-resource "aws_alb" "server_app" {
-  security_groups = ["${aws_security_group.server_app_alb.id}"]
-  subnets         = ["${module.vpc.public_subnet_ids}"]
-
-  # subnets = ["${var.vpc_subnet_ids}"]
-  name = "${var.project_id}-alb${var.environment}AppServer"
-
-  tags {
-    Name        = "albAppServer"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
-
-resource "aws_alb_target_group" "server_app_https" {
-  # Name can only be 32 characters long, so we MD5 hash the name and
-  # truncate it to fit.
-  name = "tf-tg-${replace("${md5("${var.environment}HTTPSAppServer")}", "/(.{0,26})(.*)/", "$1")}"
-
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-
-  port     = "443"
-  protocol = "HTTP"
-  vpc_id   = "${module.vpc.id}"
-
-  tags {
-    Name        = "tg${var.project_id}-${var.environment}HTTPSAppServer"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
-
-resource "aws_alb_listener" "server_app_https" {
-  load_balancer_arn = "${aws_alb.server_app.id}"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = "${var.ssl_certificate_arn}"
-
-  default_action {
-    target_group_arn = "${aws_alb_target_group.server_app_https.id}"
-    type             = "forward"
-  }
-}
-
-data "template_file" "app_server_http_ecs_task" {
+data "template_file" "ecs_potsdam_task" {
   template = "${file("task-definitions/app-server.json")}"
 
   vars {
-    nginx_image_url      = "279682201306.dkr.ecr.us-east-1.amazonaws.com/rastervision-nginx:${var.image_version}"
-    api_server_image_url = "279682201306.dkr.ecr.us-east-1.amazonaws.com/rastervision-api-server:${var.image_version}"
+    nginx_image_url      = "${var.aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/potsdam-nginx:${var.image_version}"
+    api_server_image_url = "${var.aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/potsdam-api-server:${var.image_version}"
     region               = "${var.aws_region}"
     environment          = "${var.environment}"
   }
 }
 
-resource "aws_ecs_task_definition" "app_server" {
-  family                = "${var.project_id}_${var.environment}AppServer"
-  container_definitions = "${data.template_file.app_server_http_ecs_task.rendered}"
+resource "aws_ecs_task_definition" "potsdam" {
+  family                = "${var.environment}PotsdamDemo"
+  container_definitions = "${data.template_file.ecs_potsdam_task.rendered}"
 }
 
-# Defines running an ECS task as a service
-resource "aws_ecs_service" "server" {
-  name                               = "${var.project_id}-${var.environment}Server"
-  cluster                            = "${aws_ecs_cluster.container_instance.id}"
-  task_definition                    = "${aws_ecs_task_definition.app_server.family}:${aws_ecs_task_definition.app_server.revision}"
-  desired_count                      = "${var.desired_instance_count}"
-  iam_role                           = "${aws_iam_role.container_instance_ecs.name}"
-  deployment_minimum_healthy_percent = "0"                                                                                           # allow old services to be torn down
-  deployment_maximum_percent         = "100"
+resource "aws_cloudwatch_log_group" "potsdam" {
+  name = "log${var.environment}PotsdamDemo"
 
-  load_balancer {
-    target_group_arn = "${aws_alb_target_group.server_app_https.id}"
-    container_name   = "nginx"
-    container_port   = "80"
+  tags {
+    Environment = "${var.environment}"
   }
+}
+
+module "potsdam_ecs_service" {
+  source = "github.com/azavea/terraform-aws-ecs-web-service?ref=0.2.0"
+
+  name                = "Potsdam"
+  vpc_id              = "${data.terraform_remote_state.core.vpc_id}"
+  public_subnet_ids   = ["${data.terraform_remote_state.core.public_subnet_ids}"]
+  access_log_bucket   = "${data.terraform_remote_state.core.logs_bucket_id}"
+  access_log_prefix   = "ALB/Potsdam"
+  port                = "443"
+  ssl_certificate_arn = "${var.ssl_certificate_arn}"
+
+  cluster_name                   = "${data.terraform_remote_state.core.container_instance_name}"
+  task_definition_id             = "${aws_ecs_task_definition.potsdam.family}:${aws_ecs_task_definition.potsdam.revision}"
+  desired_count                  = "${var.potsdam_ecs_desired_count}"
+  min_count                      = "${var.potsdam_ecs_min_count}"
+  max_count                      = "${var.potsdam_ecs_max_count}"
+  deployment_min_healthy_percent = "${var.potsdam_ecs_deployment_min_percent}"
+  deployment_max_percent         = "${var.potsdam_ecs_deployment_max_percent}"
+  container_name                 = "nginx"
+  container_port                 = "443"
+  health_check_path              = "/"
+  ecs_service_role_name          = "${data.terraform_remote_state.core.ecs_service_role_name}"
+  ecs_autoscale_role_arn         = "${data.terraform_remote_state.core.ecs_autoscale_role_arn}"
+
+  project     = "Potsdam Demo"
+  environment = "${var.environment}"
 }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -2,11 +2,9 @@ variable "project" {
   default = "Raster Vision"
 }
 
-variable "project_id" {
-  default = "RV"
+variable "environment" {
+  default = "Production"
 }
-
-variable "environment" {}
 
 # AWS
 
@@ -14,63 +12,48 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_availability_zones" {
-  default = ["us-east-1c", "us-east-1d"]
+variable "aws_account_id" {
+  default = "896538046175"
 }
-
-variable "aws_key_name" {}
+variable "remote_state_bucket" {
+  default = "geotrellis-site-production-config-us-east-1"
+}
 
 # ECS
 variable "image_version" {}
 
 variable "ssl_certificate_arn" {
-  default = "arn:aws:acm:us-east-1:279682201306:certificate/7c1d8d7e-b809-44a2-a835-5cdf5a0e9357"
+  default = "arn:aws:acm:us-east-1:896538046175:certificate/a416c2af-00dd-4afd-8c71-dd32edefa839"
 }
 
-variable "desired_instance_count" {}
-variable aws_ecs_ami {}
-variable ecs_instance_type {}
+variable "potsdam_ecs_desired_count" {
+  default = "1"
+}
+
+variable "potsdam_ecs_min_count" {
+  default = "1"
+}
+
+variable "potsdam_ecs_max_count" {
+  default = "2"
+}
+
+variable "potsdam_ecs_deployment_min_percent" {
+  default = "100"
+}
+
+variable "potsdam_ecs_deployment_max_percent" {
+  default = "200"
+}
 
 # IAM
-
-variable "aws_ecs_for_ec2_service_role_policy_arn" {
-  default = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-}
-
-variable "aws_ecs_service_role_policy_arn" {
-  default = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
-}
 
 variable "aws_s3_policy_arn" {
   default = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-variable "aws_cloudwatch_logs_policy_arn" {
-  default = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-}
+# CloudFront
 
-# VPC
-
-variable vpc_cidr_block {}
-variable vpc_external_access_cidr_block {}
-
-variable "vpc_private_subnet_cidr_blocks" {
-  default = ["10.0.1.0/24", "10.0.3.0/24"]
-}
-
-variable "vpc_public_subnet_cidr_blocks" {
-  default = ["10.0.0.0/24", "10.0.2.0/24"]
-}
-
-variable vpc_bastion_ami {}
-variable vpc_bastion_instance_type {}
-
-variable server_app_alb_ingress_cidr_block {
-  default = ["0.0.0.0/0"]
-}
-
-# Route53
-
-variable "route53_external_hosted_zone" {
-  default = "potsdam.geotrellis.io."
+variable "cdn_price_class" {
+  default = "PriceClass_200"
 }
